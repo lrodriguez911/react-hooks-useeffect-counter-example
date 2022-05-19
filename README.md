04f0c5d5ed3eba4a7ac7c8460d6c8c556e7676c0
lrodriguez911 <lrodriguez.arevalosc@gmail.com>
2022-05-19 11:05:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hooks-useeffect-counter-example-6xaw5b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hooks-useeffect-counter-example-6xaw5b)
---
+++ index.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("Number of clicks: " + counter);
+  });
+
+  increment() => {
+    setCounter(counter+1);
+  }
+  decrement()=>{
+    setCounter(counter-1)
+  }
+  return <div>
+  <p>{counter}</p>
+  <button onClick={increment}>Increment</button>
+  <button onClick={decrement}>Decrement</button>
+  </div>;
+}
+
+const el = <Counter />; 
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+);
